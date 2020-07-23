d861507e21281de1126653b98abdaca72e19c0b0
James Swift <sbcjames86@yahoo.com>
2020-07-23 12:03:34 -0700

    add js

---
+++ app.js
@@ -0,0 +1,41 @@
+function getuserName(){
+    var userNameInFunction = prompt('What is your name?');
+    alert('Hello There...' + userNameInFunction);
+    document.write('<h2>' + userNameInFunction + '</h2>');
+    return userNameInFunction;
+}
+var userName = getuserName();
+
+function confirmAskQuestions(){
+    confirm(userName + ' I am going to ask you some questions!');
+}
+confirmAskQuestions();
+
+function askQuestion(){
+    var userCodeAnswer = prompt('Do you have a dog? (yes or no)');
+    if (userCodeAnswer == "yes"){
+        alert('I do too');
+    } else {
+        alert('That is too bad.  We only groom dog!);
+    }
+}
+askQuestion();
+
+
+function askTime(){
+    var userTimeInFunction = prompt('What hour is it in military time 0-24?');
+    return userTimeInFunction;
+}
+var userTime = askTime();
+
+
+function checkTime(){
+    if(userTime < 12){
+        alert('Good Morning');
+    } else if (userTime >= 12 && userTime < 18){
+        alert('Good Afternoon');
+    } else {
+        alert('Good Evening');
+    }
+}
+checkTime();
